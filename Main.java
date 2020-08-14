73fa32ac32b10c8c44dbf9ab1bb25fafea1f7dd9
Janitha Missaka <49041052+Janitham97@users.noreply.github.com>
2020-08-14 12:56:55 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package package01;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MyExceptionTest m = new MyExceptionTest();
+         m.Calculation();
+    }
+}
